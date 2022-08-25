49e589b30cd450d3adf56a599ca1013bc7002cf5
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-08-25 20:12:27 -0300

    criação de migrations

---
+++ src/database/migrations/20220825215245-create-blog-post.js
@@ -0,0 +1,34 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('BlogPosts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      published: {
+        type: 'TIMESTAMP'
+      },
+      updated: {
+        type: 'TIMESTAMP'
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('BlogPosts');
+  }
+};
