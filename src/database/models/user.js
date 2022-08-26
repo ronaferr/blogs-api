6ec4b84a8e64f08cfaee0f99973ed1c359e88d86
Ronaldo Carlos <ferreiraronaldocarlos@gmail.com>
2022-08-25 23:43:29 -0300

    criação de model User

---
+++ src/database/models/user.js
@@ -0,0 +1,13 @@
+const User = (sequelize, DataTypes) => {
+  const User = sequelize.define("User", {
+    id: DataTypes.INTERGER,
+    displayName: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    image: DataTypes.STRING,
+  });
+
+  return User;
+};
+
+module.exports = User;
